48d37d3798196451fcf667ce90ef6c2e644d24ff
Prabhav Puranik <67889957+PrabhavPuranik@users.noreply.github.com>
2025-07-07 08:35:39 +0530

    Hands on-7

---
+++ Week3_Spring_Data_JPA_with_Hibernate/1. spring-data-jpa-handson/07_Add_a_new_country/Code/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,98 @@
+package com.cognizant.ormlearn;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+	private static CountryService countryService;
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+		countryService = context.getBean(CountryService.class);
+		LOGGER.info("Inside main");
+		testAddCountry();
+		try {
+			Thread.sleep(60000); // 60 seconds
+		} catch (InterruptedException e) {
+			LOGGER.error("Main thread interrupted: {}", e.getMessage());
+		}
+	}
+
+	private static void testAddCountry() {
+		LOGGER.info("Start testAddCountry");
+		Country country = new Country();
+		country.setCode("XX");
+		country.setName("Test Country");
+		try {
+			countryService.addCountry(country);
+			LOGGER.info("Country added successfully");
+			Country addedCountry = countryService.findCountryByCode("XX");
+			LOGGER.debug("Added Country={}", addedCountry);
+		} catch (CountryNotFoundException e) {
+			LOGGER.error("Find Error: {}", e.getMessage());
+		} catch (Exception e) {
+			LOGGER.error("Add Error: {}", e.getMessage());
+		}
+		LOGGER.info("End testAddCountry");
+	}
+
+	private static void getAllCountriesTest() {
+		LOGGER.info("Start");
+		try {
+			Country country = countryService.findCountryByCode("IN");
+			LOGGER.debug("Country:{}", country);
+			if ("India".equals(country.getName())) {
+				LOGGER.info("Country name is valid");
+			} else {
+				LOGGER.error("Country name is invalid");
+			}
+		} catch (CountryNotFoundException e) {
+			LOGGER.error("Error: {}", e.getMessage());
+		}
+		LOGGER.info("End");
+	}
+
+	private static void testUpdateCountry() {
+		LOGGER.info("Start testUpdateCountry");
+		Country country = new Country();
+		country.setCode("XX");
+		country.setName("Updated Test Country");
+		countryService.updateCountry(country);
+		try {
+			Country updatedCountry = countryService.findCountryByCode("XX");
+			LOGGER.debug("Updated Country={}", updatedCountry);
+		} catch (CountryNotFoundException e) {
+			LOGGER.error("Error: {}", e.getMessage());
+		}
+		LOGGER.info("End testUpdateCountry");
+	}
+
+	private static void testDeleteCountry() {
+		LOGGER.info("Start testDeleteCountry");
+		countryService.deleteCountry("XX");
+		try {
+			countryService.findCountryByCode("XX");
+			LOGGER.error("Country still exists");
+		} catch (CountryNotFoundException e) {
+			LOGGER.debug("Deleted Country does not exist, as expected");
+		}
+		LOGGER.info("End testDeleteCountry");
+	}
+
+	private static void testFindByNameContaining() {
+		LOGGER.info("Start testFindByNameContaining");
+		List<Country> countries = countryService.findByNameContaining("land");
+		LOGGER.debug("Countries={}", countries);
+		LOGGER.info("End testFindByNameContaining");
+	}
+}
