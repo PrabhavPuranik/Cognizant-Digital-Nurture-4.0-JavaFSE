755ad9e2b185cb4624ece2ba70bcb870509a72a7
Prabhav Puranik <67889957+PrabhavPuranik@users.noreply.github.com>
2025-07-26 18:13:14 +0530

    Week 6 Hands-on

---
+++ Week6_React/2. ReactJS-HOL/Code/studentapp/src/Components/Contact.js
@@ -0,0 +1,7 @@
+export function Contact(){
+    return(
+        <div>
+            <h3>Welcome to the Contact page of the Student Management Portal</h3>
+        </div>
+    );
+}
---
+++ Week6_React/2. ReactJS-HOL/Code/studentapp/src/Components/Home.js
@@ -0,0 +1,7 @@
+export function Home(){
+    return(
+        <div>
+            <h3>Welcome to the Home page of Student Management Portal</h3>
+        </div>
+    );
+}
